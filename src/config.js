21f508d635f249c83474678174e8fce46b9c8428
Sandesh Shinde <sandeshshinde7047@gmail.com>
2025-02-20 14:05:31 +0530

    EC2 backed url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-    BASE_URL:'https://vogue-backend-1.onrender.com' // Change this to your desired base URL
+    BASE_URL:'http://65.0.170.2:3000' 
 };
 
 export default config;
